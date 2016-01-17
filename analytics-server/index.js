97fc1eeb37fe6747b550a4d9eec800e4d35124fc
nathan breitsch <nathanbreitsch@n1-22-176.dhcp.drexel.edu>
2016-01-17 06:31:35 -0500

    bower components

--- analytics-server/index.js
+++ analytics-server/index.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var app = express();
 var data = require('./data/json/countries.json');
+var serveStatic = require('serve-static')
+
 var analytics = require('./analytics.js');
 
 var dc = new analytics.DataContext(data);
@@ -9,14 +11,50 @@ var dc = new analytics.DataContext(data);
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(serveStatic(__dirname + '/client'));
 
 var version = 0;
 var message = {type: 'none'}
 
-app.use(express.static('client'));
+//hard core hacking, serve all static files like this
+app.get('/bower_components/jquery/dist/jquery.min.js', function (req, res) {
+  res.sendFile('client/bower_components/jquery/dist/jquery.min.js', { root: __dirname })
+});
+
+app.get('/bower_components/bootstrap/dist/js/bootstrap.min.js', function (req, res) {
+  res.sendFile('client/bower_components/bootstrap/dist/js/bootstrap.min.js', { root: __dirname })
+});
+
+app.get('/bower_components/knockout/dist/knockout.js', function (req, res) {
+  res.sendFile('client/bower_components/knockout/dist/knockout.js', { root: __dirname })
+});
+
+app.get('/bower_components/reqwest/src/reqwest.js', function (req, res) {
+  res.sendFile('client/bower_components/reqwest/src/reqwest.js', { root: __dirname })
+});
+
+app.get('/bower_components/highcharts/highcharts.js', function (req, res) {
+  res.sendFile('client/bower_components/highcharts/highcharts.js', { root: __dirname })
+});
+
+app.get('/app.js', function (req, res) {
+  res.sendFile('client/app.js', { root: __dirname })
+});
+
+app.get('/app.css', function (req, res) {
+  res.sendFile('client/app.css', { root: __dirname })
+});
+
+app.get('/bower_components/knockout/dist/knockout.js', function (req, res) {
+  res.sendFile('client/bower_components/knockout/dist/knockout.js', { root: __dirname })
+});
+
+app.get('/bower_components/bootstrap/dist/css/bootstrap.min.css', function (req, res) {
+  res.sendFile('client/bower_components/bootstrap/dist/css/bootstrap.min.css', { root: __dirname })
+});
 
 app.get('/', function (req, res) {
-  res.sendFile('client/index.html')
+  res.sendFile('client/index.html', { root: __dirname })
 });
 
 app.post('/question', function(req, res){
